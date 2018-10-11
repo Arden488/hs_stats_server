740d138de24b59b0afc539c4e07c56ce11158c19
Anton Samoilov <absamoilov88@gmail.com>
2018-10-11 18:17:19 +0300

    Fill in schema for graphql

--- schema.js
+++ schema.js
@@ -1,27 +1,98 @@
 export default `
+scalar Date
+
 type Archetype {
   _id: String
   name: String
-  class: String
+  charClass: String
   code: String
 }
+type Game {
+  _id: String,
+  deckId: String,
+  datetime: Date,
+  opponentClass: String,
+  opponentArchetype: String,
+  outcome: String
+}
+type WinrateCard {
+  cardId: String,
+  wins: Int,
+  losses: Int
+}
+type Winrate {
+  _id: String,
+  deckId: String,
+  opponentArchetypeId: String,
+  opponentClass: String,
+  wins: Int,
+  losses: Int,
+  games: Int,
+  cards: [WinrateCard]
+}
+type Deck {
+  _id: String,
+  name: String,
+  charClass: String,
+  code: String
+}
+
+input WinrateCardInput {
+  cardId: String!
+}
+
 type Query {
   allArchetypes(
     name: String,
-    class: String,
+    charClass: String,
     code: String,
     key_features: String
   ): [Archetype!]!,
+  allWinrates(
+    deckId: String,
+    opponentArchetypeId: String,
+    opponentClass: String,
+    wins: Int,
+    losses: Int,
+    games: Int,
+    cards: [WinrateCardInput]
+  ): [Winrate!]!,
+  getWinratesByClass(
+    deckId: String,
+    opponentClass: String
+  ): [Winrate!]!,
   getArchetype(
     id: String!
-  ): Archetype!
+  ): Archetype!,
+  allDecks(
+    name: String,
+    charClass: String,
+    code: String
+  ): [Deck!]!,
+  getDeck(
+    id: String!
+  ): Deck!
 }
+
 type Mutation {
   createArchetype(
     name: String!,
-    class: String!,
+    charClass: String!,
     code: String!,
     key_features: String!
   ): Archetype!
+  createGame(
+    deckId: String!,
+    opponentClass: String!,
+    opponentArchetype: String!,
+    outcome: String!
+  ): Game!
+  updateWinrate(
+    deckId: String!,
+    opponentClass: String!, 
+    opponentArchetype: String!,
+    outcome: String!,
+    cards: [WinrateCardInput]
+  ): Winrate!
 }
 `
