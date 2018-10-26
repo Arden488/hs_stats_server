1f4861a9cf3f2112669fc7ba5d68e69351059bf8
Anton Samoilov <absamoilov88@gmail.com>
2018-10-27 02:22:29 +0300

    Add model for opponent's decks

--- src/models/oppdecks.js
+++ src/models/oppdecks.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema
 
 const OppDeck = mongoose.model('OppDeck', {
   name: String,
   charClass: String,
-  archetypeId: String,
+  archetypeId: { type: Schema.Types.ObjectId, ref: 'Archetype' },
   code: String,
   key_features: String
 })
